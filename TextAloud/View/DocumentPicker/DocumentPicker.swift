311fcdac992ac5df32781c45830c20e5b217cd76
Taananas <t.rybalkina@brightlab.me>
2023-02-15 00:33:38 +0400

    update DocumentPicker

--- TextAloud/View/DocumentPicker/DocumentPicker.swift
+++ TextAloud/View/DocumentPicker/DocumentPicker.swift
@@ -10,58 +10,63 @@ import UIKit
 import MobileCoreServices
 import SNDocx
 
-struct DocumentPicker: UIViewControllerRepresentable {
-    @Binding var fileContent: String
-    
-    func makeCoordinator() -> DocumentPickerCoordinator {
-        return DocumentPickerCoordinator(fileContent: $fileContent)
-    }
-    
-    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
-        let controller: UIDocumentPickerViewController
-        
-        if #available(iOS 14, *) {
-            controller = UIDocumentPickerViewController(forOpeningContentTypes: [.content, .rtf, .text], asCopy: true)
-        } else {
-            controller = UIDocumentPickerViewController(documentTypes: [String(kUTTypeRTF), String(kUTTypeText), String(kUTTypeData)], in: .import)
-        }
-        controller.delegate = context.coordinator
-        return controller
-    }
-    
-    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {}
-}
 
-class DocumentPickerCoordinator: NSObject, UIDocumentPickerDelegate, UINavigationControllerDelegate {
+#warning("Need to add iCloud to capabilities")
+// An error with Icloud "[DocumentManager] Failed to associate thumbnails for picked URL.."
+// Need to add iCloud to capabilities
+
+struct DocumentPicker: UIViewControllerRepresentable {
+  
     @Binding var fileContent: String
-    
-    init(fileContent: Binding<String>) {
-        _fileContent = fileContent
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
     }
-    
-    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-        let fileURL = urls[0]
-        print(fileURL)
-        let temp = NSData(contentsOf: fileURL)
-        if let tryForString = try? NSAttributedString(data: temp! as Data, options: [
-            .documentType: NSAttributedString.DocumentType.rtf,
-            .characterEncoding: String.Encoding.utf8.rawValue
-            ], documentAttributes: nil) {
-            fileContent = tryForString.string.withoutTags
-            return
-        }
-        if let tryForString = try? NSAttributedString(data: temp! as Data, options: [
-            .documentType: NSAttributedString.DocumentType.plain,
-            .characterEncoding: String.Encoding.utf8.rawValue
-            ], documentAttributes: nil) {
-            fileContent = tryForString.string.withoutTags
-            return
-        }
-        if let result = SNDocx.shared.getText(fileUrl: urls[0]) {
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(forOpeningContentTypes: [.content, .rtf, .text], asCopy: true)
         
-            fileContent = result.withoutTags
-            print(result)
-            return
+        picker.allowsMultipleSelection = false
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: DocumentPicker.UIViewControllerType, context: UIViewControllerRepresentableContext<DocumentPicker>) {
+    }
+
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+
+        var parent: DocumentPicker
+
+        init(_ parent: DocumentPicker){
+            self.parent = parent
+
+        }
+        
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            let fileURL = urls[0]
+            print(fileURL)
+            if let temp = NSData(contentsOf: fileURL) as? Data {
+                if let tryForString = try? NSAttributedString(data: temp, options: [
+                    .documentType: NSAttributedString.DocumentType.rtf,
+                    .characterEncoding: String.Encoding.utf8.rawValue
+                    ], documentAttributes: nil) {
+                    parent.fileContent = tryForString.string.withoutTags
+                    return
+                }
+                if let tryForString = try? NSAttributedString(data: temp, options: [
+                    .documentType: NSAttributedString.DocumentType.plain,
+                    .characterEncoding: String.Encoding.utf8.rawValue
+                    ], documentAttributes: nil) {
+                    parent.fileContent = tryForString.string.withoutTags
+                    return
+                }
+            }
+
+            if let result = SNDocx.shared.getText(fileUrl: urls[0]) {
+                parent.fileContent = result.withoutTags
+                return
+            }
         }
     }
 }
---
+++ TextAloud/View/RootView/TestTextView.swift
@@ -0,0 +1,110 @@
+//
+//  TestTextView.swift
+//  TextAloud
+//
+//  Created by Богдан Зыков on 14.02.2023.
+//
+
+import SwiftUI
+import AVFAudio
+struct TestTextView: View {
+    @State var text = "test test test test test test. test test test test test test"
+    @State var range: NSRange?
+    @State var str: String = ""
+    @State var selectedRange: NSRange?
+    var body: some View {
+        VStack {
+            //if let range{
+                Text(str)
+           // }
+            
+            TestTextUIView(str: $str, selectedRange: $selectedRange, text: text)
+              
+        }
+        .padding(.top, 60)
+    }
+}
+
+struct TestTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestTextView()
+    }
+}
+
+
+struct TestTextUIView: UIViewRepresentable {
+    
+    @Binding var str: String
+    @Binding var selectedRange: NSRange?
+    let text: String
+
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        textView.delegate = context.coordinator
+        textView.isEditable = false
+        textView.isSelectable = false
+        textView.isUserInteractionEnabled = true
+        
+       
+        
+       
+        
+        
+        let gesture = UITapGestureRecognizer(target: context.coordinator,
+                                                     action: #selector(Coordinator.handleTap))
+        textView.addGestureRecognizer(gesture)
+        
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        let attrStr = NSMutableAttributedString(string: text)
+        
+        if let selectedRange{
+            attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor(.red), range: selectedRange)
+        }
+     
+        attrStr.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: CGFloat(20), weight: .heavy), range: NSRange(location: 0, length: attrStr.length))
+        
+        uiView.attributedText = attrStr
+    }
+    
+    func makeCoordinator() -> Coordinator {
+         Coordinator(self)
+    }
+    
+    class Coordinator : NSObject, UITextViewDelegate {
+        
+        var parent: TestTextUIView
+     
+        
+        init(_ uiTextView: TestTextUIView) {
+            self.parent = uiTextView
+        }
+        
+        func textView(_ textView: UITextView, shouldInteractWith textAttachment: NSTextAttachment, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+            parent.str = "tap"
+            return true
+        }
+        
+        @objc func handleTap(_ sender: UITapGestureRecognizer) {
+            
+            if let textView = sender.view as? UITextView{
+                let textLength = textView.textStorage.length
+                let point = sender.location(in: textView)
+                let glyphIndex: Int? = textView.layoutManager.glyphIndex(for: point, in: textView.textContainer, fractionOfDistanceThroughGlyph: nil)
+                let index: Int? = textView.layoutManager.characterIndexForGlyph(at: glyphIndex ?? 0)
+                
+               
+                guard let characterIndex = index, characterIndex < textLength else {
+                    return
+                }
+                
+                parent.str = "\(textView.textStorage.length)"
+
+                parent.selectedRange = Helpers.getParagraphRangeForLocation(characterIndex, textView.textStorage.string)
+            }
+        }
+    }
+}
+
