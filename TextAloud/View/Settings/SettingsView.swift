d50ac558fe1e15a2b076736cdc7e02197d1e090b
Taananas <t.rybalkina@brightlab.me>
2023-02-14 14:23:25 +0400

    adding new folders, adding extensions, adding services

--- TextAloud/View/Settings/SettingsView.swift
+++ TextAloud/View/Settings/SettingsView.swift
@@ -8,28 +8,7 @@
 import SwiftUI
 import UIKit
 
-extension Color: RawRepresentable {
-    public init?(rawValue: String) {
-        guard let data = Data(base64Encoded: rawValue) else{
-            self = .black
-            return
-        }
-        do{
-            let color = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UIColor ?? .black
-            self = Color(color)
-        }catch{
-            self = .black
-        }
-    }
-    public var rawValue: String {
-        do{
-            let data = try NSKeyedArchiver.archivedData(withRootObject: UIColor(self), requiringSecureCoding: false) as Data
-            return data.base64EncodedString()
-        }catch{
-            return ""
-        }
-    }
-}
+
 
 struct SettingsView: View {
     @AppStorage("selectedColor") var selectedColor: Color = Color(UIColor(red: 0.96, green: 0.9, blue: 0.258, alpha: 0.4))
