c6b27cc3d10a2d7aace900fb86b9566a598553d6
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-03-28 16:10:40 +0100

    [MINOR] Settings redesign

--- TextAloud/View/Settings/SettingsView.swift
+++ TextAloud/View/Settings/SettingsView.swift
@@ -7,12 +7,45 @@
 
 import SwiftUI
 
+struct SettingsLink<Content: View>: View {
+    var label: String
+    var pageLink: String?
+    @ViewBuilder var content: Content
+    
+    var body: some View {
+        if let url = pageLink, let fullUrl = URL(string: url) {
+            
+            
+            Link(destination: fullUrl) {
+                HStack{
+                    Text(label)
+                        .font(.callout.weight(.medium))
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                }
+            } } else {
+                
+                
+                
+                NavigationLink {
+                    content
+                } label: {
+                    Text(label)
+                        .font(.callout.weight(.medium))
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                }}
+    }
+}
+
 struct SettingsView: View {
     @ObservedObject var speech: SpeechSynthesizer
     @ObservedObject var settingVM: SettingViewModel
     @ObservedObject var storeKitManager: StoreKitManager
+    var onPurchaseClick: () -> ()
+    
     @Environment(\.dismiss) var dismiss
-        
+    
     var body: some View {
         Group{
             if #available(iOS 16.0, *) {
@@ -30,20 +63,13 @@ struct SettingsView: View {
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        Group{
-            SettingsView(speech: SpeechSynthesizer(), settingVM: SettingViewModel(), storeKitManager: StoreKitManager())
+        Text("Test").sheet(isPresented: .constant(true)) {
+            SettingsView(speech: SpeechSynthesizer(), settingVM: SettingViewModel(), storeKitManager: StoreKitManager(), onPurchaseClick: {})
                 .environment(\.locale, .init(identifier: "en"))
-//          SettingsView(speech: SpeechSynthesizer(), settingVM: SettingViewModel())
-//                .environment(\.locale, .init(identifier: "fr"))
-//            SettingsView(speech: SpeechSynthesizer(), settingVM: SettingViewModel())
-//                .environment(\.locale, .init(identifier: "zh_Hant_HK"))
-//            SettingsView(speech: SpeechSynthesizer(), settingVM: SettingViewModel())
-//                .environment(\.locale, .init(identifier: "de"))
-//            SettingsView(speech: SpeechSynthesizer(), settingVM: SettingViewModel())
-//                .environment(\.locale, .init(identifier: "es"))
         }
     }
 }
+
 
 
 //MARK: - Setting view container
@@ -51,13 +77,52 @@ extension SettingsView{
     
     private var viewContainer: some View{
         ScrollView(.vertical, showsIndicators: false) {
-            VStack(spacing: 16) {
-                aboutGroupView
-                customizationGroupView
-                voiceAllowanceView
-                HelpsBlockView()
-            }
-            .padding(.horizontal)
+            VStack {
+                Group{
+                    GroupBox {
+                        SettingsLink(label: "Voice Selection") {
+                            LanguageSpeechView()
+                                .environmentObject(speech)
+                                .environmentObject(settingVM)
+                        }
+                        Divider().padding(.vertical, 4)
+                        SettingsLink(label: "Voice Usage Stats") {
+                            voiceAllowanceView
+                        }
+                        Divider().padding(.vertical, 4)
+                        SettingsLink(label: "Text Display Customisation") {
+                            customizationGroupView
+                        }
+                    }
+                }
+                Spacer().padding(.vertical, 2)
+                
+                Group {
+                    GroupBox {
+                        SettingsLink(label:"Documentation", pageLink: "https://docs.acecentre.org.uk/products/v/textaloud/") {}
+                        Divider().padding(.vertical, 4)
+                        SettingsLink(label:"Find out more about Ace Centre", pageLink: "https://acecentre.org.uk") {}
+                        Divider().padding(.vertical, 4)
+                        SettingsLink(label:"Contact Us", pageLink: "https://acecentre.org.uk/contact") {}
+                        Divider().padding(.vertical, 4)
+                        SettingsLink(label:"Recieve our Newsletter", pageLink: "https://acecentre.org.uk/?newsletter=textaloud") {}
+                    }
+                    Spacer().padding(.vertical, 2)
+}
+                
+                Group {
+                    GroupBox {
+                        HStack{
+                            Text("Version")
+                                .font(.callout.weight(.medium))
+                            Spacer()
+                            Text(Bundle.main.appVersionShort)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+            }.padding(.horizontal)
+            
         }
         .navigationBarTitleDisplayMode(.large)
         .navigationTitle(Localization.settings.toString)
@@ -103,27 +168,45 @@ extension SettingsView {
     }
     
     private var voiceAllowanceView: some View {
-        Group{
-            GroupBox(label: SettingsLabelView(labelText: "Voice Allowance", labelImage: "clock")) {
-                Divider().padding(.vertical, 4)
-                HStack{
-                    Text("Used")
-                        .font(.callout.weight(.medium))
-                    Spacer()
-                    Text(timeUsedInMinutesLabel)
-                        .foregroundColor(.secondary)
+        VStack{
+            Group{
+                GroupBox {
+                    HStack{
+                        Text("Used")
+                            .font(.callout.weight(.medium))
+                        Spacer()
+                        Text(timeUsedInMinutesLabel)
+                            .foregroundColor(.secondary)
+                    }
+                    Divider().padding(.vertical, 4)
+                    HStack{
+                        Text("Left")
+                            .font(.callout.weight(.medium))
+                        Spacer()
+                        Text(timeLeftInMinutesLabel)
+                            .foregroundColor(.secondary)
+                    }
                 }
-                Divider().padding(.vertical, 4)
-                HStack{
-                    Text("Left")
-                        .font(.callout.weight(.medium))
-                    Spacer()
-                    Text(timeLeftInMinutesLabel)
-                        .foregroundColor(.secondary)
-                }
+                
             }
-                                              
-        }
+            if storeKitManager.hasPurchasedUnlimitedVoiceAllowance == false {
+                Button(action: {
+                    onPurchaseClick()
+                }) {
+                    Text("Purchase Unlimited Time")
+                        .frame(maxWidth: .infinity)
+                        .font(.headline)
+                        .padding(10)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                .buttonBorderShape(.capsule)
+                .padding(.top)
+            }
+            Spacer().frame(maxHeight: .infinity)
+        }.padding(.horizontal)
+            .navigationBarTitleDisplayMode(.large)
+            .navigationTitle("Voice Allowance")
     }
 }
 
@@ -143,56 +226,61 @@ extension SettingsView{
     }
     
     private var customizationGroupView: some View{
-        GroupBox(label: SettingsLabelView(labelText: Localization.customization.toString, labelImage: "paintbrush")) {
-            Divider().padding(.vertical, 4)
-            Text(Localization.aboutCustomization.toString)
-                .padding(.vertical, 8)
-                .font(.footnote)
-                .multilineTextAlignment(.leading)
-            
-            Divider().padding(.vertical, 4)
-            VStack {
-                Text(Localization.textWillLook.toString)
-                    .hLeading()
-                HStack {
-                    Text(settingVM.getNewString())
-                        .font(.system(size: CGFloat(settingVM.fontSize)))
-                        .fontWeight(.heavy)
-                        .padding()
-                }
-                .hCenter()
-                .background(.white)
-                .cornerRadius(8)
-                .padding()
-            }
-            Divider().padding(.vertical, 4)
-            
-            Group{
-                ColorPicker(Localization.pickSelection.toString, selection: $settingVM.selectedColor, supportsOpacity: true)
-                
+        VStack {
+            GroupBox() {
+                Text(Localization.aboutCustomization.toString)
+                    .fixedSize(horizontal: false, vertical: true)
                 Divider().padding(.vertical, 4)
                 
-                ColorPicker(Localization.pickHighlight.toString, selection: $settingVM.readingColor, supportsOpacity: true)
                 
-                Divider().padding(.vertical, 4)
-                
-                Stepper {
-                    HStack {
-                        Text(Localization.fontSize.toString)
-                        Spacer()
-                        Text("\(settingVM.fontSize)")
+                Group{
+                    ColorPicker(Localization.pickSelection.toString, selection: $settingVM.selectedColor, supportsOpacity: true)
+                    
+                    Divider().padding(.vertical, 4)
+                    
+                    ColorPicker(Localization.pickHighlight.toString, selection: $settingVM.readingColor, supportsOpacity: true)
+                    
+                    Divider().padding(.vertical, 4)
+                    
+                    Stepper {
+                        HStack {
+                            Text(Localization.fontSize.toString)
+                            Spacer()
+                            Text("\(settingVM.fontSize)")
+                        }
+                    } onIncrement: {
+                        settingVM.incrementStep()
+                    } onDecrement: {
+                        settingVM.decrementStep()
                     }
-                } onIncrement: {
-                    settingVM.incrementStep()
-                } onDecrement: {
-                    settingVM.decrementStep()
+                }
+                .font(.headline.weight(.medium))
+                Divider().padding(.vertical, 4)
+                VStack {
+                    Text(Localization.textWillLook.toString)
+                        .hLeading()
+                    HStack {
+                        Text(settingVM.getNewString())
+                            .font(.system(size: CGFloat(settingVM.fontSize)))
+                            .fontWeight(.heavy)
+                            .padding()
+                    }
+                    .hCenter()
+                    .background(.white)
+                    .cornerRadius(8)
+                    .padding()
                 }
             }
-            .font(.headline.weight(.medium))
+            Spacer().frame(maxHeight: .infinity)
         }
+        .padding(.horizontal)
+        .navigationBarTitleDisplayMode(.large)
+        .navigationTitle("Text Display Customisation")
+        
+        
     }
     
-   
+    
     
     private var languageLink: some View{
         NavigationLink {
@@ -206,15 +294,4 @@ extension SettingsView{
             Image(systemName: "chevron.right")
         }
     }
-    
-//    private var helpLink: some View{
-//        NavigationLink {
-//            HelpsView()
-//        } label: {
-//            Text("Help")
-//                .font(.callout.weight(.medium))
-//            Spacer()
-//            Image(systemName: "chevron.right")
-//        }
-//    }
 }
