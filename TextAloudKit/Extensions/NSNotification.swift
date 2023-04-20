092053416b05098d878c27d54b2905aaba594e8b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-04-20 12:03:26 +0100

    Refactor NSNotification extension

---
+++ TextAloudKit/Extensions/NSNotification.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotification.swift
+//  TextAloudKit
+//
+//  Created by Gavin Henderson on 20/04/2023.
+//
+
+import Foundation
+
+extension NSNotification {
+    static public let OnStopSpeech = Notification.Name.init("OnStopSpeech")
+}
