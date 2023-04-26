6f877bb012c6bd058a6c867809c4ed5bb010b952
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-04-26 11:33:25 +0100

    Move helper to filereader

--- TextAloudKit/Models/FileReader.swift
+++ TextAloudKit/Models/FileReader.swift
@@ -1,18 +1,19 @@
 //
-//  Helpers.swift
-//  TextAloud
+//  FileReader.swift
+//  TextAloudKit
 //
+//  Created by Gavin Henderson on 26/04/2023.
 //
 
 import Foundation
 import PDFKit
 
-final class Helpers{
-
+final class FileReader {
+    
    static func pdfToText(for url: URL) -> String?{
        let docContent = NSMutableAttributedString()
        guard let pdf = PDFDocument(url: url) else {return nil}
-       for i in 1 ..< pdf.pageCount {
+       for i in 0 ..< pdf.pageCount {
            guard let page = pdf.page(at: i) else { continue }
            guard let pageContent = page.attributedString else { continue }
            docContent.append(pageContent)
@@ -29,5 +30,17 @@ final class Helpers{
     static func rtfToText(for url: URL) -> String?{
         guard let attributedStringWithPlain: NSAttributedString = try? NSAttributedString(url: url, options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.rtf], documentAttributes: nil) else { return nil}
         return attributedStringWithPlain.string
+    }
+}
+
+public enum TextAloudFileType: String{
+    case rtf, pdf, txt
+    
+    public func getText(for url: URL) -> String?{
+        switch self {
+        case .rtf: return FileReader.rtfToText(for: url)
+        case .pdf: return FileReader.pdfToText(for: url)
+        case .txt: return FileReader.plainToText(for: url)
+        }
     }
 }
